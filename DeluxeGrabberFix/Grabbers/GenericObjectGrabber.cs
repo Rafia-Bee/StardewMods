43c1a3537b06e3f5bb53b5305898e36495092727
RafiaBushra <saiyuriye.rafia@gmail.com>
2026-03-03 19:58:23 +0200

    Fix Issue #20: Replace hardcoded seed/forage IDs with game APIs
    - SeedTreeGrabber: Use HarvestInterceptor + tree.shake() instead of
      hardcoded seed ID switch. Supports modded trees (e.g. Pine Spruce mod),
      SeedDropItems, SeedItemId, Mystery Box, Qi Beans, and ShakeItems.
    
    - GenericObjectGrabber: Remove redundant ForageableItems {430},
      GetBeachForageItems {393,397,152}, and Location is Beach check.
      All were already covered by isForage() which checks categories
      -79/-81/-80/-75/-23, forage_item context tag, and (O)430.
      Automatically supports SVE and other mods with proper forage tags.

--- DeluxeGrabberFix/Grabbers/GenericObjectGrabber.cs
+++ DeluxeGrabberFix/Grabbers/GenericObjectGrabber.cs
@@ -1,15 +1,11 @@
-using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using DeluxeGrabberFix.Framework;
 using StardewValley;
-using StardewValley.Locations;
 
 namespace DeluxeGrabberFix.Grabbers;
 
 internal class GenericObjectGrabber : ObjectsMapGrabber
 {
-    private static readonly HashSet<int> ForageableItems = new() { 430 };
-
     public GenericObjectGrabber(ModEntry mod, GameLocation location)
         : base(mod, location)
     {
@@ -29,22 +25,11 @@ internal class GenericObjectGrabber : ObjectsMapGrabber
         return false;
     }
 
-    private HashSet<int> GetBeachForageItems()
-    {
-        return new HashSet<int> { 393, 397, 152 };
-    }
-
     private bool IsGrabbable(Object obj)
     {
         if (obj.bigCraftable.Value)
             return false;
 
-        if (obj.isForage() && obj.ParentSheetIndex != 73)
-            return true;
-
-        if (Location is Beach && GetBeachForageItems().Contains(obj.ParentSheetIndex))
-            return true;
-
-        return ForageableItems.Contains(obj.ParentSheetIndex);
+        return obj.isForage() && obj.ParentSheetIndex != 73;
     }
 }
